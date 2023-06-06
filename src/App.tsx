b89cbaab5a1b973aeedc39047376e40e9f475523
cremirdevio <josephajibodu@gmail.com>
2023-06-06 21:03:35 +0100

    search input createear

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from "./layout"
+import Home from "./pages/home"
+import NotFound from "./pages/not-found"
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+        <div className="h-36 flex justify-center items-center bg-gray-100">
+      <input
+        type="text"
+        placeholder="Enter Zip"
+        className="px-8 py-4 text-gray-900 text-2xl font-bold outline-4 transition-[outline] duration-200 outline-gray-600/20 focus:outline focus:rounded-md "
+      />
+    </div>
+    </div>
+  );
+}
