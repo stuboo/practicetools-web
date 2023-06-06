9d6e60d9da449aee7bff2aef6c813092c62dd9e4
cremirdevio <josephajibodu@gmail.com>
2023-06-06 22:49:32 +0100

    user interface designed

---
+++ src/pages/home/PhysicalTherapyCard.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+export default function PhysicalTherapyCard() {
+  return (
+    <div className="border-2 border-gray-300 rounded-md bg-white p-4 transition-[border] hover:border-gray-600">
+      <h2 className="text-2xl text-gray-900 font-bold">
+        Community Health Network
+      </h2>
+      <div className="flex text-lg">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+          />
+        </svg>
+
+        <p>
+          3125 S. Scatterfield Road{' '}
+          <span className="text-sm text-gray-500">Anderson, IN</span>{' '}
+        </p>
+        <span className="bg-gray-800 text-white rounded-lg px-2 ml-3 text-sm flex justify-center items-center">
+          46013
+        </span>
+      </div>
+      <div className="flex text-sm gap-4 text-gray-600 lowercase">
+        <p>PatientExperience@eCommunity.com</p>
+        <p>317-621-7000</p>
+      </div>
+
+      <div className="flex gap-6 mt-8">
+        <a
+          href="https://www.ecommunity.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          <span className="flex gap-1">
+            {' '}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+              />
+            </svg>
+            https://www.ecommunity.com/
+          </span>
+        </a>
+
+        <span>(317) 860-7668</span>
+      </div>
+      <p className="italic text-gray-500">Notes: Multiple locations</p>
+    </div>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,25 @@
+import PhysicalTherapyCard from './PhysicalTherapyCard'
+
 export default function Home() {
   return (
-    <div>
+    <div className="h-full flex flex-col bg-red">
       <div className="h-36 flex justify-center items-center bg-gray-100">
         <input
           type="text"
           placeholder="Enter Zip"
           className="px-8 py-4 text-gray-900 text-2xl font-bold outline-4 transition-[outline] duration-200 outline-gray-600/20 focus:outline focus:rounded-md "
         />
+      </div>
+
+      {/* <div className="px-10 py-12 grid grid-cols-2 gap-4">
+        <PhysicalTherapyCard />
+        <PhysicalTherapyCard />
+        <PhysicalTherapyCard />
+        <PhysicalTherapyCard />
+      </div> */}
+
+      <div className="flex justify-center items-center h-80">
+        <p className="text-2xl">Not Found!</p>
       </div>
     </div>
   )
