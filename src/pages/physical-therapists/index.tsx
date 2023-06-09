d9711535d67c2268bcaa633ae517c63c88a1ccb4
cremirdevio <josephajibodu@gmail.com>
2023-06-09 21:37:22 +0100

    drawer working with the crud list

--- src/pages/physical-therapists/index.tsx
+++ src/pages/physical-therapists/index.tsx
@@ -1,10 +1,22 @@
 import { useQuery } from 'react-query'
 import PhysicalTherapistAPI from '../../api/physicaltherapist'
+import { Dialog } from '@headlessui/react'
+import { useState } from 'react'
+import { TherapistType } from '../home/types'
 
 export default function PhysicalTherapistLists() {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+  const [selectedTherapist, setSelectedTherapist] =
+    useState<TherapistType | null>(null)
+
   const { data: therapists } = useQuery('therapists', () =>
     PhysicalTherapistAPI.getAll()
   )
+
+  const handleShowFullDetails = (therapist: TherapistType) => {
+    setSelectedTherapist(therapist)
+    setIsSideBarOpen(true)
+  }
 
   return (
     <>
@@ -106,17 +118,217 @@ export default function PhysicalTherapistLists() {
                     </a>
                   </td>
                   <td className="px-6 py-4">
-                    <a
-                      href="#"
+                    <button
+                      type="button"
+                      onClick={() => handleShowFullDetails(therapist)}
                       className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                     >
-                      Edit
-                    </a>
+                      View
+                    </button>
                   </td>
                 </tr>
               ))}
           </tbody>
         </table>
+
+        {/* Full Details Dialog */}
+        <Dialog
+          open={isSideBarOpen}
+          onClose={() => setIsSideBarOpen(false)}
+          className="relative z-50 h-full"
+        >
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+          <div className="fixed inset-0 md:left-3/4  bg-white">
+            <Dialog.Panel className={'h-full flex flex-col'}>
+              <button
+                className="absolute right-4 top-4 rounded-full p-1 hover:bg-gray-300"
+                onClick={() => setIsSideBarOpen(false)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+              <div className="h-24 bg-gray-200 flex gap-4 items-center px-8">
+                <button className="flex items-center gap-1 bg-blue-500 px-4 py-1 rounded hover:bg-blue-500/80 text-white">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                    />
+                  </svg>
+                  Edit
+                </button>
+
+                <button className="flex items-center gap-1 bg-red-500 px-4 py-1 rounded hover:bg-red-500/80 text-white">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                  Delete
+                </button>
+              </div>
+              <div className="p-4 overflow-y-auto h-80 grow">
+                <Dialog.Title
+                  as={'h3'}
+                  className="text-base font-semibold leading-7 text-gray-900"
+                >
+                  {selectedTherapist?.name}
+                </Dialog.Title>
+                <Dialog.Description
+                  as="p"
+                  className="mt-1 max-w-2xl text-sm leading-6 text-gray-500"
+                >
+                  {selectedTherapist?.city},{selectedTherapist?.state}
+                </Dialog.Description>
+                <div className="mt-6 border-t border-gray-100">
+                  <dl className="divide-y divide-gray-100">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Name
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.name}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Email Address
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.email}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Phone
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.phone}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Address 1
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.address}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Address 2
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.address_two}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Zip Code
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.zip}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Website
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.website}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Fax
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.fax}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Medicare Status
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.medicare_status ? 'Yes' : 'No'}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Medicaid Status
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.medicaid_status ? 'Yes' : 'No'}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Cash Only
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.cash_only ? 'Yes' : 'No'}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Referral From URL
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.referral_form_url}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Expectation Letter URL
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.expectations_letter_url}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900">
+                        Note
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        {selectedTherapist?.notes}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </div>
+        </Dialog>
       </div>
     </>
   )
