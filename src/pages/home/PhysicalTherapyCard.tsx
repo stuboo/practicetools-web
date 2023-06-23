a5b50b5bca19212b69e86ca47953e451e1277f28
cremirdevio <josephajibodu@gmail.com>
2023-06-23 17:48:50 +0100

    zipcode search working fine

--- src/pages/home/PhysicalTherapyCard.tsx
+++ src/pages/home/PhysicalTherapyCard.tsx
@@ -8,6 +8,15 @@ interface PhysicalTherapyCardProps {
 export default function PhysicalTherapyCard({
   therapist,
 }: PhysicalTherapyCardProps) {
+  const handleAddressClick = () => {
+    const addressQuery = encodeURIComponent(
+      `${therapist.address}, ${therapist.city}, ${therapist.state} ${therapist.zip}`
+    )
+    const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${addressQuery}`
+
+    window.open(googleMapsUrl, '_blank')
+  }
+
   return (
     <div className="border-2 border-gray-300 rounded-md bg-white p-4 transition-[border] hover:border-gray-600">
       <h2 className="text-2xl text-gray-900 font-bold">{therapist.name}</h2>
@@ -32,25 +41,25 @@ export default function PhysicalTherapyCard({
           />
         </svg>
 
-        <p>
+        <p className="cursor-pointer" onClick={handleAddressClick}>
           {/* TODO: clickable and open to google map address in another tab */}
           {therapist.address}
           <span className="text-sm text-gray-500 ml-2">
             {therapist.city}, {therapist.state}
           </span>{' '}
         </p>
-        <span className="bg-gray-800 text-white rounded-lg px-2 ml-3 text-sm flex justify-center items-center">
+        <span className="bg-gray-500 text-white rounded-lg px-2 py-1 ml-3 text-sm flex justify-center items-center">
           {therapist.zip}
         </span>
-        <span className="bg-gray-800 text-white rounded-lg px-2 ml-3 text-sm flex justify-center items-center">
-          {therapist.distance}
+        <span className="bg-blue-800 text-white rounded-lg px-2 py-1 ml-3 text-sm flex justify-center items-center">
+          {Number(therapist.distance).toFixed(1)} miles
         </span>
         {/* Add another badge that show distance e.g 2.4 miles */}
       </div>
       <div className="text-sm gap-4 text-gray-600">
         <p>Email: {therapist.email}</p>
         <a
-          href="https://www.ecommunity.com/"
+          href={therapist.website ?? ''}
           target="_blank"
           rel="noopener noreferrer"
           className="hover:underline"
@@ -71,7 +80,7 @@ export default function PhysicalTherapyCard({
                 d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
               />
             </svg>
-            https://www.ecommunity.com/
+            {therapist.website ?? ''}
           </span>
         </a>
         <p>Tel: {therapist.phone}</p>
@@ -80,10 +89,24 @@ export default function PhysicalTherapyCard({
 
       <div className="flex gap-6 mt-8">
         {/* Button to download referral form */}
-        <Button
-          iconLeft={<MdOutlineDownload size={24} />}
-          title="Referral Form"
-        />
+        {therapist.referral_form_url && (
+          <a href={therapist.referral_form_url} target="_blank">
+            <Button
+              iconLeft={<MdOutlineDownload size={24} />}
+              title="Referral Form"
+              colorScheme="blue"
+              disabled={therapist.referral_form_url ? false : true}
+            />
+          </a>
+        )}
+        {!therapist.referral_form_url && (
+          <Button
+            iconLeft={<MdOutlineDownload size={24} />}
+            title="Referral Form"
+            colorScheme="blue"
+            disabled={true}
+          />
+        )}
       </div>
       {/* <p className="italic text-gray-500">Fax: {therapist.fax}</p>
       <p className="italic text-gray-500">Notes: {therapist.notes}</p> */}
