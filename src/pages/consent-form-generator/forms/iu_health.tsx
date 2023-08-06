db820ce56a3af00808b356f5b64005bc07be2e58
cremirdevio <josephajibodu@gmail.com>
2023-08-07 00:01:59 +0100

    date format on form change to day/month/year

--- src/pages/consent-form-generator/forms/iu_health.tsx
+++ src/pages/consent-form-generator/forms/iu_health.tsx
@@ -87,7 +87,7 @@ export default function IUHealthForm({ procedures }: IUHealthFormProps) {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     const year = date.getFullYear();
-    return `${month}/${day}/${year}`;
+    return `${day}/${month}/${year}`;
   };
 
   const formatTime = (timeInput: string|undefined) => {
