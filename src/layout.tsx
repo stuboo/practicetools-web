b7593e3bce31e6c1447cfd1c90ab8c9e4cf1834d
Ryan Stewart <ryan@ryanstewart.com>
2025-04-01 20:44:09 -0500

    Hide View Backend button on scheduling page

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,11 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 const navigation: { name: string; href: string; current: boolean }[] = []
 
 export default function Layout() {
+  const location = useLocation();
+  const isSchedulingPage = location.pathname === '/scheduling';
+
   return (
     <div className="h-full">
       <nav className="bg-white border-gray-200 dark:bg-gray-900 border-b-2">
@@ -18,13 +21,15 @@ export default function Layout() {
             </span>
           </Link>
           <div className="flex md:order-2">
-            <Link
-              to={'/physical-therapists'}
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              View Backend
-            </Link>
+            {!isSchedulingPage && (
+              <Link
+                to={'/physical-therapists'}
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                View Backend
+              </Link>
+            )}
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
@@ -54,7 +59,7 @@ export default function Layout() {
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               {navigation.map((nav, index) => (
-                <li>
+                <li key={index}>
                   <Link
                     to={nav.href}
                     className={
@@ -63,7 +68,6 @@ export default function Layout() {
                         : 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
                     }
                     aria-current={nav.current ? 'page' : undefined}
-                    key={index}
                   >
                     {nav.name}
                   </Link>
